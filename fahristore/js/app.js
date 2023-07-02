988212961f37487a1b1ac5ed0a00c432e346995a
CHLLEXANDEV <108337657+ChlleXanDev@users.noreply.github.com>
2023-07-02 23:15:27 +0700

    Delete fahristore directory

--- fahristore/js/app.js
+++
@@ -1,6 +0,0 @@
-//Navbar hamburger
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function () {
-  navbar = document.querySelector(".navbar");
-  navbar.classList.toggle("active");
-}
